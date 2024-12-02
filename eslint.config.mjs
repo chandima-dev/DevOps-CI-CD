fc311b089088510ff253fb648fa3cffc9808a9c9
chandima-dev <sanjusooriyaarachchi99@gmail.com>
2024-12-02 10:17:50 +0530

    update eslint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,11 +1,11 @@
 import globals from 'globals';
-// import pluginJs from '@eslint/js';
 
 /** @type {import('eslint').Linter.Config[]} */
 export default {
   languageOptions: {
     globals: {
       ...globals.node,  // Include all Node.js globals (e.g., require, module)
+      structuredClone: 'readonly',  // Add structuredClone as a global variable
     },
     parserOptions: {
       ecmaVersion: 12,  // ECMAScript 2021 syntax
@@ -13,10 +13,9 @@ export default {
     },
   },
   rules: {
-    // Manually add the recommended ESLint rules here
-    'no-undef': 'error', // example rule, ensure undefined variables are caught
-    'no-unused-vars': 'warn', // warn about unused variables
-    'eqeqeq': 'error', // enforce strict equality
+    'no-undef': 'error',  // example rule, ensure undefined variables are caught
+    'no-unused-vars': 'warn',  // warn about unused variables
+    'eqeqeq': 'error',  // enforce strict equality
     // You can add more recommended rules manually or according to your needs
   },
 };
